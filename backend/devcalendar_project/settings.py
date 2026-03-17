0bb9b57dcd9835e8326e480a6950278c3b04d64e
Thanasis <thanasis20007@gmail.com>
2026-03-17 16:14:07 +0200

    Initial commit

--- backend/devcalendar_project/settings.py
+++ backend/devcalendar_project/settings.py
@@ -4,6 +4,9 @@ Calendar App Desktop Django Settings
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Calendar-App-Desktop_project.urls'
+ROOT_URLCONF = 'devcalendar_project.urls'
 
 TEMPLATES = [
     {
@@ -58,7 +61,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Calendar-App-Desktop_project.wsgi.application'
+WSGI_APPLICATION = 'devcalendar_project.wsgi.application'
 
 # Database - PostgreSQL
 DATABASES = {
